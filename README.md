96d3c83653640f10017eb805afb03f21a6f07637
vityok <vityok@localhost>
2018-11-23 19:13:38 +0200

    updated comments, readme

--- README.md
+++ README.md
@@ -6,6 +6,12 @@ Video player GUI interface.
 Developed as an excercise in C++ programming. Uses GTK for UI, OMX
 Player for playing Video on Raspberry Pi, MPlayer on desktop.
 
+Goal is to create a lightweight and fast GUI for the OMXPlayer app on
+Raspberry Pi. It will not have the overhead of Python with its bloat
+of libraries and bindings, and at the same time will offer a minimal
+UI to conveniently playback videos with hardware acceleration and high
+quality by the OMX Player.
+
 Work in progress.
 
 Depends on gtkmm library version 3.0 or higher. To install run:
@@ -14,3 +20,8 @@ Depends on gtkmm library version 3.0 or higher. To install run:
 
 If you are building on a Ubuntu (might even work on Debian) or
 Raspbian system.
+
+Links
+-----
+
+* [OMX Player home](https://github.com/popcornmix/omxplayer)
--- src/omx_player_proxy.cpp
+++ src/omx_player_proxy.cpp
@@ -1,3 +1,6 @@
+#include <giomm.h>
+#include <glibmm.h>
+
 #include <iostream>
 #include <list>
 #include <string>
@@ -8,6 +11,24 @@ using std::string;
 
 const string OmxPlayerProxy::PROG_NAME = "omxplayer";
 
+// todo: it might be more convenient and reasonable to use the DBus
+// API for interaction with the running OMX Player application
+//
+// The API reference can be found at:
+//
+// http://transit.iut2.upmf-grenoble.fr/doc/glibmm-2.4/reference/html/group__DBus.html
+//
+// And example application at:
+//
+// https://github.com/GNOME/glibmm/tree/master/examples/dbus
+//
+// The C API is documented at:
+//
+// https://developer.gnome.org/gio/stable/gdbus-convenience.html
+//
+// And a tutorial at:
+//
+// https://dbus.freedesktop.org/doc/dbus-tutorial.html
 void OmxPlayerProxy::launch_player(const string& fname) {
   std::cout << "omxplayer::launch_player" << std::endl;
 
